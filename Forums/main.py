de471e3f91d10e52a82581e3bda10cd194ee493c
Unknown <redateur13@gmail.com>
2018-03-01 06:20:17 +0100

    add update and get_by_name function

--- Forums/main.py
+++ Forums/main.py
@@ -1,26 +1,60 @@
-
 import models 
 import store 
 
-member1 = models.Member("reda" , 27)
-member2 = models.Member("amine" , 18)
+def create_members():
+	member1 = models.Member("reda" , 27)
+	member2 = models.Member("amine" , 18)
+	print(member1)
+	print(member2)
+	print(member2.age)
+	print("=" * 20)
+	
+	return member1, member2
+	
+def creat_posts():	
+	post1 = models.Post("function Print in Python " , "The print statement has ...")
+	post2 = models.Post("python input " , "Input can come in various ..." )
+	post3 = models.Post("exercise " , "Create a program that asks the user ...")
+	print(post1.title)
+	print(post2.title)
+	print(post3.topic)
+	
+	return post1, post2, post3
+	
+	
+def add_members_to_store(members_instances, member_stored):
+	for member in members_instances:
+		member_store.add(member)
+		
+def print_all_members(member_store):
+	print("=" * 20)
+	
+	for member in member_store.get_all():
+		print(member)
+	print("=" * 20)		
+	
+def update_object(member_store, member2):
+	member2_copy = models.Member(member2.name, member2.age)
+	member2_copy.id = 3
 
+	if member2_copy is not member2:
+		print("member2 and member2_copy are not the same !")	
+		print(member2_copy)
+		member2_copy.name = "john"
+		member_store.update(member2_copy)
+		print(member_store.get_by_id(member2.id))
+		
+members_instances = create_members()
+member1, member2, member3 = members_instances
 
-post1 = models.Post("function Print in Python " , "The print statement has ...")
-post2 = models.Post("python input " , "Input can come in various ..." )
-post3 = models.Post("exercise " , "Create a program that asks the user ...")
+member_store = stores.MemberStore()
 
+add_members_to_store(members_instances, member_store)
 
-print(member1.name)
-print(post1.title)
-print(post1.topic)
+print_all_members(member_store)
 
-print(member2.age)
-print(post2.title)
-print(post3.topic)
+update_object(member_store, member2)
 
-member_stored = store.MemberStore()
-member_stored.add(member1)
-member_stored.add(member2)
+print_all_members(member_store)		
 
-print(member_stored.get_all)
+	
--- Forums/store.py
+++ Forums/store.py
@@ -3,34 +3,54 @@ class MemberStore:
 	last_id = 1
 
 	def get_all(self):
-		# get all members
 		return MemberStore.members
+		
 
 	def add(self, member):
-		# append member
 		member.id = MemberStore.last_id
 		MemberStore.members.append(member)
 		MemberStore.last_id += 1
-      
+		
   
 	def get_by_id(self, id):
 		 member_list = self.get_all()
+		 result = None
 		 for member in member_list:
 			 if member.id == id:
-				 return member
-		 return None		 
-
+				 result = member
+		 return result	
+		 	 
 	   
 	def delete(self, id):
-		# delete member by id
-		for member in self.get_all():
-			if member is not None:
-				MemberStore.members.remove(member)
-		
+		member = self.get_by_id(id)
+		MemberStore.members.remove(member)	
+				
 		   
 	def entity_exists(self, member):
-		# checks if an entity exists in a store
 		result = True
 		if self.get_by_id(member.id) is None:
 			result = False
-		return result
+		return result	
+		
+	
+	def update(self, member):
+		member_list = self.get_all()
+		for index, this_member in enumerate(member_list):
+			if this_member.id == member.id:
+				member_list[index] = member
+				
+				
+	def get_by_name (self, name):
+		result = []
+		member_list = self.get_all()
+		for member in member_list:
+			if member.name == name:
+				result.append(member)
+		return result		
+				
+				
+			
+		
+		
+
+
